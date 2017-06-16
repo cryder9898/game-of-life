bebbd5a658d58073c8505de75df4e2bd6229f530
Christopher Ryder <cryder9898@gmail.com>
2017-06-16 10:41:59 -0400

    added Gameboard component

---
+++ src/GameBoard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Cell from './Cell';
+
+class GameBoard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      board: [],
+      col: 20,
+      row: 15,
+    }
+  }
+
+  // initializes board
+  componentWillMount() {
+    this.resetBoard();
+  }
+
+  resetBoard = () => {
+    let board = [];
+    for (let i = 0; i < this.state.col; i++) {
+      let row = [];
+      for (let j = 0; j < this.state.row; j++) {
+        row.push(0);
+      }
+      board.push(row);
+    }
+    console.log(board);
+    this.setState({board: board});
+  }
+
+
+  render() {
+    return (
+      <div className='board'>
+        {this.state.board.map((col, i) => {
+          return col.map((row, j) => {
+            return (
+              <Cell key={[i, j]}/>
+            );
+          })
+        })}
+      </div>
+    );
+  }
+}
+
+export default GameBoard;
